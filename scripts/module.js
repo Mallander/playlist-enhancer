0b8ac9b97bd5581bd126551b2dc7005a375b18da
Chris Wilde <Mallander@users.noreply.github.com>
2021-06-20 23:23:37 +0100

    Included scripts file

---
+++ scripts/module.js
@@ -0,0 +1,161 @@
+// Add "Curate" button when a sound is playing and set the click event for dialog pop-up.
+// Initiate Drop & Drop functionality for song movement between playlists
+Hooks.on('renderPlaylistDirectory', (app, html) => {
+  
+	//Set default values for: dialog content and dialog box placement
+	let curateDialogContent = '<div class="form-group"><label>Name</label><div class="form-fields"><input type="text" name="name" id="curated-name" placeholder="New Playlist" required=""></div></div>';
+	let leftOffset = $(document).width() - 320 - 310;
+
+	// Set dialog options for placement
+	const dialogOptions = {
+		width: 320,
+		top: 140,
+		left: leftOffset
+	};
+
+	// Create Dialog and save for later
+	const curateDialog = new Dialog({
+		title: 'Curate Playlist',
+		content: curateDialogContent,
+		buttons: {
+			button1: {
+				label: "Create Playlist",
+				callback: (html) => {combinePlaylist(html)},
+				icon: `<i class="fas fa-check"></i>`
+			},
+		}
+	}, dialogOptions)
+
+  let qcButton = '<button class="create-entity quick-combine" title="Quick Combine"><i class="fas fa-compress-arrows-alt"></i>Curate</button>';
+  html.find('#currently-playing .playlist-header').append(qcButton);
+  html.find('.quick-combine').on('click', (e) => {
+  	curateDialog.render(true);
+  })
+
+
+  //Set the drag targets
+  const sounds = html.find(".directory-list .sound-name");
+
+  // Get the sound IDs and set draggable, set ondragend callback
+  sounds.each((index, el) => {
+  	try {
+		const soundId = el.closest("li.sound").getAttribute('data-sound-id');
+
+		if (!soundId) {
+			return;
+		}
+
+		el.draggable = true;
+		el.ondragend = (e) => moveSoundToPlaylist(soundId, e);
+	} catch (e) {
+		console.error(`Error: ${e}: Unable to make song ${el} draggable`);
+	}
+  })
+
+  
+});
+
+// Copy sound from one playlist to another
+function moveSoundToPlaylist(soundId, event) {
+
+	// Playlist ID that the drag was started from
+	let startingPlaylistId = getPlaylistIdFromElement(event.srcElement.closest(".playlist"));
+	// The sound object being dragged
+	let soundObject = getSoundObjectFromId(soundId);
+	// The target playlist ID from the drop co-ordinates
+	let targetPlaylistId = getPlaylistIdFromElement(document.elementFromPoint(event.clientX, event.clientY))
+	
+	// If drop target is not a playlist, throw error
+	if(!targetPlaylistId) {
+		ui.notifications.error("Error: Target must be a playlist");
+		return;
+	} else if (startingPlaylistId == targetPlaylistId) { // Check if the drop target is the same as the starting playlist
+		return;
+	}
+
+	// Add song to new playlist
+	addSongToPlaylist(soundObject, game.playlists.entities.find(p => p.id === targetPlaylistId))
+
+}
+
+// Credit to https://github.com/gsimon2 for creating this function for use in playlist-drag-and-drop: https://github.com/gsimon2/playlist-drag-and-drop
+function getSoundObjectFromId(soundId) {
+	try {
+		const playlistSounds = Array.from(game.playlists.values()).flatMap((playlist) => Array.from(playlist.sounds.values()));
+		return playlistSounds.find(playlistSound => playlistSound.id === soundId);
+	} catch (e) {
+		console.error(`Error: Failed to get sound object from soundId: ${soundId}. Error: ${e}`);
+		return '';
+	}
+}
+
+// Return the playlist ID from the DOM element if applicable, otherwise return false
+function getPlaylistIdFromElement(el) {
+	if((el.closest(".playlist") == undefined || el.closest(".playlist") == null) && !el.classList.contains("playlist")) {
+		return false;
+	}
+	return el.classList.contains("playlist") ? el.getAttribute("data-entity-id") : el.closest(".playlist").getAttribute("data-entity-id");
+}
+
+
+// Function for the playlist combination
+async function combinePlaylist(formHtml) {
+
+	// Set Variables:
+	// Playlist name: string, form input
+	// Playing sounds: array, list of currently playing sounds
+	// Playlist: playlist, checks if the name's in use
+	let playlistName = formHtml.find("#curated-name").val();
+	let playingSounds = game.playlists.directory._playingSounds
+	let playlist = game.playlists.entities.find(p => p.name === playlistName)
+
+	// Check if sounds are playing
+	if(playingSounds.length == 0) {
+		ui.notifications.warn("No tracks are currently playing, start building your atomsphere first.");
+		return;
+	}
+
+	// Check if playlist name is in use already
+	if(playlist != undefined || playlist != null) {
+		ui.notifications.error("Playlist already exists with the name '" + playlistName + "'.");
+		return;
+	}
+
+	// Begin Curation
+	ui.notifications.info("Curating playlist...")
+
+	// Create empty playlsit
+	await Playlist.create({
+	    "name": playlistName,
+	    "permission": {
+	        "default": 0,
+	    },
+	    "flags": {},
+	    "sounds": [],
+	    "mode": 2,
+	    "playing": false,
+	});
+
+	// Get the playlist object we can inject songs into
+	playlist = game.playlists.entities.find((p) => p.name === playlistName)
+
+	// Check if playlist has created properly.
+	if(playlist == undefined || playlist == null) {
+		ui.notifications.error("Error creating playlist - playlist entity not found post-creation");
+		return;
+	}
+
+	// Loop currently playing sounds and add them to the empty playlist
+	for(let i = 0; i < playingSounds.length; i++) {
+		await addSongToPlaylist(playingSounds[i], playlist)
+	}
+
+	// Complete curation
+	ui.notifications.info("Playlist '" + playlistName + "' created successfully!")
+}
+
+// Add song object to new playlist object with existing song settings
+async function addSongToPlaylist(song, playlist) {
+
+	playlist.createEmbeddedEntity("PlaylistSound", { name: song.name, path: song.path, repeat: song.repeat, volume: song.volume, fade: song.fade == undefined ? 1000 : song.fade }, {});
+}
