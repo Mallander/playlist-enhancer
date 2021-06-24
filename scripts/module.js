f9548a97feae9db6ac26377f43232cbf77584542
Chris Wilde <Mallander@users.noreply.github.com>
2021-06-24 23:12:31 +0100

    Added settings and refactored some old code
    Fixed an issue where curating mutiple tracks at once may cause some to not play afterwards.
    Improved functionality around adding songs to playlist. Affects and improves all areas of the module.
    Fixed compatibility with Maestro module button placement.
    Added module settings for:
    - Enable/Disable copying of modules between playlists. Disabling will delete the original and "move" the track.
    - Enable/Disable playlist status coloring and icons
    - Set playlist status colors for "Playing" and "Paused" tracks

--- scripts/module.js
+++ scripts/module.js
@@ -1,10 +1,14 @@
-// Add "Curate" button when a sound is playing and set the click event for dialog pop-up.
-// Initiate Drop & Drop functionality for song movement between playlists
-Hooks.on('renderPlaylistDirectory', (app, html) => {
-  
+
+import { registerModuleSettings } from "./settings.js";
+
+var curateDialog;
+
+Hooks.on("init", () => {
+
+	var leftOffset = $(document).width() - 320 - 310;
+
 	//Set default values for: dialog content and dialog box placement
 	let curateDialogContent = '<div class="form-group"><label>Name</label><div class="form-fields"><input type="text" name="name" id="curated-name" placeholder="New Playlist" required=""></div></div>';
-	let leftOffset = $(document).width() - 320 - 310;
 
 	// Set dialog options for placement
 	const dialogOptions = {
@@ -14,7 +18,7 @@ Hooks.on('renderPlaylistDirectory', (app, html) => {
 	};
 
 	// Create Dialog and save for later
-	const curateDialog = new Dialog({
+	curateDialog = new Dialog({
 		title: 'Curate Playlist',
 		content: curateDialogContent,
 		buttons: {
@@ -26,31 +30,58 @@ Hooks.on('renderPlaylistDirectory', (app, html) => {
 		}
 	}, dialogOptions)
 
-  let qcButton = '<button class="create-entity quick-combine" title="Quick Combine"><i class="fas fa-compress-arrows-alt"></i>Curate</button>';
-  html.find('#currently-playing .playlist-header').append(qcButton);
-  html.find('.quick-combine').on('click', (e) => {
-  	curateDialog.render(true);
-  })
+	registerModuleSettings();
 
 
-  //Set the drag targets
-  const sounds = html.find(".directory-list .sound-name");
+})
 
-  // Get the sound IDs and set draggable, set ondragend callback
-  sounds.each((index, el) => {
-  	try {
-		const soundId = el.closest("li.sound").getAttribute('data-sound-id');
+// Add "Curate" button when a sound is playing and set the click event for dialog pop-up.
+// Initiate Drop & Drop functionality for song movement between playlists
+Hooks.on('renderPlaylistDirectory', (app, html) => {
+  	let qcButton = '<button class="quick-combine" title="Quick Combine"><i class="fas fa-compress-arrows-alt"></i>Curate</button>';
+	
+	html.find('#currently-playing .playlist-header').append(qcButton);
+	html.find('.quick-combine').on('click', (e) => {
+		curateDialog.render(true);
+	})
 
-		if (!soundId) {
-			return;
-		}
+	if(game.settings.get("playlist-enhancer", "enablePlaylistStates")) {
+		// Detect paused sounds and add classes
+		let pausedSounds = $("#playlists .directory-list .sound-control[title='Resume Sound']")
+		pausedSounds.closest(".sound.flexrow").addClass("paused-sound");
+		pausedSounds.closest(".playlist").addClass("paused-playlist");
+		pausedSounds.closest(".subdirectory").siblings(".folder-header").addClass("paused-folder");
 
-		el.draggable = true;
-		el.ondragend = (e) => moveSoundToPlaylist(soundId, e);
-	} catch (e) {
-		console.error(`Error: ${e}: Unable to make song ${el} draggable`);
+		// Add playing sounds CSS classes
+		$("#playlists .directory-list .playlist.playing").closest(".subdirectory").siblings(".folder-header").addClass("playing-folder");
+		$("#playlists .directory-list .playlist.playing").addClass("playing-playlist");
+		$("#playlists .directory-list .sound.playing").addClass("playing-sound");
+
+		// Add playing icon to playing folder
+		$('<a style="padding-right: 5px;"><i class="fas fa-volume-up fa-fw"></i></a>').insertAfter(".playing-folder h3 i:first-child")
+
+		// Add paused icon to paused folder
+		$('<a style="padding-right: 5px;"><i class="fas fa-pause fa-fw"></i></a>').insertAfter(".paused-folder:not(.playing-folder) h3 i:first-child")
 	}
-  })
+
+	//Set the drag targets
+	const sounds = html.find(".directory-list .sound-name");
+
+	// Get the sound IDs and set draggable, set ondragend callback
+	sounds.each((index, el) => {
+		try {
+			const soundId = el.closest("li.sound").getAttribute('data-sound-id');
+
+			if (!soundId) {
+				return;
+			}
+
+			el.draggable = true;
+			el.ondragend = (e) => moveSoundToPlaylist(soundId, e);
+		} catch (e) {
+			console.error(`Error: ${e}: Unable to make song ${el} draggable`);
+		}
+	})
 
   
 });
@@ -64,7 +95,7 @@ function moveSoundToPlaylist(soundId, event) {
 	let soundObject = getSoundObjectFromId(soundId);
 	// The target playlist ID from the drop co-ordinates
 	let targetPlaylistId = getPlaylistIdFromElement(document.elementFromPoint(event.clientX, event.clientY))
-	
+
 	// If drop target is not a playlist, throw error
 	if(!targetPlaylistId) {
 		ui.notifications.error("Error: Target must be a playlist");
@@ -73,8 +104,12 @@ function moveSoundToPlaylist(soundId, event) {
 		return;
 	}
 
-	// Add song to new playlist
-	addSongToPlaylist(soundObject, game.playlists.entities.find(p => p.id === targetPlaylistId))
+	let playlist = game.playlists.entities.find(p => p.id === targetPlaylistId)
+	playlist.createEmbeddedDocuments('PlaylistSound', [soundObject.data]);
+
+	if(!game.settings.get("playlist-enhancer", "enableDragSongCopy")) {
+		soundObject.delete();
+	}
 
 }
 
@@ -126,54 +161,6 @@ async function beginCombinePlaylist(formHtml) {
 
 }
 
-async function createCombinePlaylist(playlistName, playingSounds, playlist, overwrite) {
-
-	// Begin Curation
-	ui.notifications.info("Curating playlist...")
-
-	if(!overwrite) {
-		// Create empty playlsit
-		await Playlist.create({
-		    "name": playlistName,
-		    "permission": {
-		        "default": 0,
-		    },
-		    "flags": {},
-		    "sounds": [],
-		    "mode": 2,
-		    "playing": false,
-		});
-
-		// Get the playlist object we can inject songs into
-		playlist = game.playlists.entities.find((p) => p.name === playlistName)
-	}
-
-	// Check if playlist has created properly.
-	if(playlist == undefined || playlist == null) {
-		ui.notifications.error("Error creating playlist - playlist entity not found post-creation");
-		return;
-	}
-
-	// Loop currently playing sounds and add them to the empty playlist
-	for(let i = 0; i < playingSounds.length; i++) {
-		await addSongToPlaylist(playingSounds[i], playlist)
-	}
-
-
-	if(overwrite) {
-		// Complete curation (overwritten)
-		ui.notifications.info("Playlist '" + playlistName + "' updated successfully!")
-	} else {
-		// Complete curation
-		ui.notifications.info("Playlist '" + playlistName + "' created successfully!")
-	}
-}
-
-// Add song object to new playlist object with existing song settings
-async function addSongToPlaylist(song, playlist) {
-	playlist.createEmbeddedEntity("PlaylistSound", { name: song.data.name, path: song.data.path, repeat: song.data.repeat, volume: song.data.volume, fade: song.data.fade == undefined ? 1000 : song.data.fade }, {});
-}
-
 async function checkOverwriteExistingPlaylist(playlistName, playingSounds, playlist) {
 
 	// Check if playlist name is in use already
@@ -195,4 +182,50 @@ async function checkOverwriteExistingPlaylist(playlistName, playingSounds, playl
 		createCombinePlaylist(playlistName, playingSounds, playlist, false);
 	}
 	
+}
+
+async function createCombinePlaylist(playlistName, playingSounds, playlist, overwrite) {
+
+	// Begin Curation
+	ui.notifications.info("Curating playlist...")
+
+	const songsToAdd = playingSounds.map((sound) => sound.toObject());
+
+	for(var key in songsToAdd) {
+		if (!songsToAdd.hasOwnProperty(key)) continue;
+
+    	var obj = songsToAdd[key];
+		for (var prop in obj) {
+		    if (Object.prototype.hasOwnProperty.call(obj, prop)) {
+		        if (!obj.hasOwnProperty(prop)) continue;
+
+		        if(prop == "playing") { obj[prop] = false }
+		        if(prop == "fade") { obj[prop] = obj[prop] == undefined ? 1000 : obj[prop] }
+		    }
+		}
+	}
+
+	if(!overwrite) {
+		// Create empty playlsit
+		await Playlist.create({
+		    "name": playlistName,
+		    "permission": {
+		        "default": 0,
+		    },
+		    "flags": {},
+		    "sounds": songsToAdd,
+		    "mode": 2,
+		    "playing": false,
+		});
+	} else {
+		playlist.createEmbeddedDocuments('PlaylistSound', songsToAdd);
+	}
+
+	if(overwrite) {
+		// Complete curation (overwritten)
+		ui.notifications.info("Playlist '" + playlistName + "' updated successfully!")
+	} else {
+		// Complete curation
+		ui.notifications.info("Playlist '" + playlistName + "' created successfully!")
+	}
 }
